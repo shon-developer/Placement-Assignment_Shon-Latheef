5ca253f2a9b65523a8e542e674c31185bf3722e0
shon <shonbwd@gmail.com>
2023-10-03 11:29:32 +0530

    callback and arrow function

---
+++ javaScriptQA/script.js
@@ -0,0 +1,53 @@
+//? 139 Callback And Arrow Function Introduction in Array
+/*
+function isEven(element) {
+  if (element % 2 === 0) {
+    return true;
+  }
+  return false;
+}
+console.log(isEven(31));
+*/
+/*
+const isEven = (value) => {
+  if (value % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(isEven(98));
+
+const arr = [25, 36, 49, 79, 101];
+console.log(arr.every(isEven));
+*/
+// create a function to check if the given number is odd or not
+/*
+const isOdd = (number) => {
+  if (number % 2 !== 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(isOdd(101));
+*/
+/*
+const arr = [78, 59, 14, 22, 203];
+const result = arr.every((item) => {
+  return item % 2 === 0;
+});
+console.log(result);
+
+// every is an array method the checks all the items in the array
+*/
+//? 140 Fill And Filter In Array In JavaScript
+//? 141 Slice and splice in array
+//? 142 Object In JavaScript
+//? 143 Methods And Objects In JavaScript
+//? 144 For Loop Basics In JavaScript
+//? 145 While And Do Loops In JavaScript
+//? 146 For Each Loops In JavaScript
+//? 147 For In And For Of Loop In JavaScript
+//? 148 Confusing Part Of This In JavaScript
+//? 149 What is DOM
